4134dd7ef4a9b5c0863d3a70f454c2ca8d01c68d
Gnomet26 <Gnometerminal026@gmail.com>
2025-08-08 22:53:04 +0500

    Update urls.py

--- Users/urls.py
+++ Users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, RefreshToken_, LogoutView
+from .views import RegisterView, LoginView, RefreshToken_, LogoutView, TestView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register_view'),
     path('login/', LoginView.as_view(), name='login_view'),
     path('refresh/', RefreshToken_.as_view(), name='refresh_token'),
     path('logout/', LogoutView.as_view(), name='logout_view'),
+    path('test/', TestView.as_view(), name = "test_view"),
 ]
