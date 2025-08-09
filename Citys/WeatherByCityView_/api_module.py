9306e8d88528295a4e2955c4fba693963c32c305
Gnomet26 <Gnometerminal026@gmail.com>
2025-08-09 23:17:31 +0500

    Update api_module.py

--- Citys/WeatherByCityView_/api_module.py
+++ Citys/WeatherByCityView_/api_module.py
@@ -1,10 +1,7 @@
 import threading
 import requests
 from .matches.get_best_match import GetBestMatch
-
-API_KEY = "0ac0279ee0c026f781b30a8d26752d9a"
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-LIST_FILE_PATH = "Citys/WeatherByCityView_/matches/city_aliases.json"
+from .secret import API_KEY, BASE_URL, LIST_FILE_PATH
 
 def fetch_weather(city):
     params = {
