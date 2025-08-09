78aa5a25ef43529c037db04e857fe0d48a0040e5
Gnomet26 <Gnometerminal026@gmail.com>
2025-08-09 23:18:48 +0500

    Update secret.py

--- Citys/WeatherByCityView_/secret.py
+++ Citys/WeatherByCityView_/secret.py
@@ -1,3 +1,3 @@
-API_KEY = "0ac0279ee0c026f781b30a8d26752d9a"
+API_KEY = "OpenWeatherMap API KEY"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 LIST_FILE_PATH = "Citys/WeatherByCityView_/matches/city_aliases.json"
